90f67ba8687662f0c113d4519755e0946bf5fe27
SamZ Ota <sam.otb@hotmail.ca>
2018-05-08 22:02:28 -0400

    first commit

---
+++ samz-class-recalculator.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Samz Recalculator Main Class
+ *
+ * @package SAMZRecalculator
+ * @author  Samer Alotaibi <sam.otb@hotmail.ca>
+ * @version GIT: $Id$
+ */
+if (!defined('ABSPATH')) {
+    die;
+}
+
+
+final class SAMZRecalculator
+{
+
+}
